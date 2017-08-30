4c860ca2ebaafad1387780b9c06e7c90e4abdade
nikita <nikitos-009@yandex.ru>
2017-08-30 16:40:59 +0300

    task_1:module_1

---
+++ Task 1/module_2/js/app.js
@@ -0,0 +1,20 @@
+var textareaCounter = document.getElementById('js-message');
+
+textareaCounter.addEventListener('keyup', function(e) {
+	var totalCount = textareaCounter.value.length;
+	document.getElementById('js-message-left-total').innerHTML = totalCount;
+}, true);
+
+
+
+textareaCounter.addEventListener('keydown', ban_sybmols, false);
+
+function ban_sybmols(evt){
+	var totalCount = textareaCounter.value.length;
+	if(totalCount > 139){
+		if(!(evt.keyCode == 8 || evt.keyCode == 45 || evt.keyCode == 46 || (evt.keyCode > 36 && evt.keyCode < 41))){
+			evt.preventDefault();
+		}
+	}
+	return false;
+}
